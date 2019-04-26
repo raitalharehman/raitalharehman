ba8c9c72b86443cdcfb6ef5be1579768c287ae9d
raitalharehman <talharaay@gmail.com>
2019-04-26 19:43:10 +0500

    SAVEPOINT

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -12,50 +12,42 @@ class Projects extends Component {
         document.title = "Projects - Rai Talha Rehman Khan";
     }
 
-  render() {
-    return (
-        <div className="projects-page">
-            <NavBar page="projects"/>
-            <div className="projects-container">
-                <h2>Projects</h2>
-                <div className="project">
-                    <picture className="project-image">
-                        <source srcSet="img/webp/pakixah.webp" type="image/webp" />
-                        <source srcSet="img/jpg/pakixah.jpg" type="image/jpeg" /> 
-                        <img src="img/jpg/pakixah.jpg" alt="New Zealand Police Logo" />
-                    </picture>
-                    <div className="project-text">
-                        <h3>Pakixah <a href="https://pakixah.com/" target="_blank" rel="noreferrer noopener"><i className="fas fa-link"></i></a></h3>
-                        <p>Its a Portal base on News, Blog, Forum, Social wall, Heros, Videos, Petitions & Polls</p>
+    render() {
+        return (
+            <div className="projects-page">
+                <NavBar page="projects" />
+                <div className="projects-container">
+                    <h2>Projects</h2>
+                    <div className="project">
+                        <img src="https://i.imgur.com/vQsy7iGh.png" alt="Pakixah" />
+                        <div className="project-text">
+                            <h3>Pakixah <a href="https://pakixah.com/" target="_blank" rel="noreferrer noopener"><i className="fas fa-link"></i></a></h3>
+                            <p>Its a Portal base on News, Blog, Forum, Social wall, Heros, Videos, Petitions & Polls</p>
+                        </div>
+                        <div className="project-tags">
+                            <div className="tag tag-css">MaterializeCSS</div>
+                            <div className="tag tag-js">JS</div>
+                            <div className="tag tag-mysql">MySQL</div>
+                            <div className="tag tag-html">Laravel</div>
+                        </div>
                     </div>
-                    <div className="project-tags">
-                        <div className="tag tag-css">MaterializeCSS</div>
-                        <div className="tag tag-js">JS</div>
-                        <div className="tag tag-mysql">MySQL</div>
-                        <div className="tag tag-html">Laravel</div>
+                    <h2>Contributions</h2>
+                    <div className="project">
+                        <img src="https://i.imgur.com/fnJ2p84h.png" alt="Saleor Store front" />
+                        <div className="project-text">
+                            <h3>Saleor Store front <a href="https://pwa.getsaleor.com/" target="_blank" rel="noreferrer noopener"><i className="fas fa-link"></i></a> <a href="https://github.com/mirumee/saleor-storefront" target="_blank" rel="noreferrer noopener"><span className="fab fa-github"></span></a></h3>
+                            <p>A GraphQL-powered, React, PWA, single-page application storefront for Saleor.</p>
+
+                        </div>
+                        <div className="project-tags">
+                            <div className="tag tag-pwa">PWA</div>
+                            <div className="tag tag-react">React</div>
+                            <div className="tag tag-js">Typescript</div>
+                            <div className="tag tag-nodejs">Node JS</div>
+                            <div className="tag tag-mysql">GraphQL</div>
+                        </div>
                     </div>
-                </div>
-                <h2>Contributions</h2>
-                <div className="project">
-                    <picture className="project-image">
-                        <source srcSet="img/webp/saloer.webp" type="image/webp" />
-                        <source srcSet="img/jpg/saleor.jpg" type="image/jpeg" /> 
-                        <img src="img/jpg/saloer.jpg" alt="Saleor Store front" />
-                    </picture>
-                    <div className="project-text">
-                        <h3>Saleor Store front <a href="https://pwa.getsaleor.com/" target="_blank" rel="noreferrer noopener"><i className="fas fa-link"></i></a> <a href="https://github.com/mirumee/saleor-storefront" target="_blank" rel="noreferrer noopener"><span className="fab fa-github"></span></a></h3>
-                        <p>A GraphQL-powered, React, PWA, single-page application storefront for Saleor.</p>
-                        
-                    </div>
-                    <div className="project-tags">
-                        <div className="tag tag-pwa">PWA</div>
-                        <div className="tag tag-react">React</div>
-                        <div className="tag tag-js">Typescript</div>
-                        <div className="tag tag-nodejs">Node JS</div>
-                        <div className="tag tag-mysql">GraphQL</div>
-                    </div>
-                </div>
-                {/* 
+                    {/* 
                 <div className="project">
                     <picture className="project-image">
                         <source srcSet="img/webp/iourpg-website.webp" type="image/webp" />
@@ -173,10 +165,10 @@ class Projects extends Component {
                     </div>
                 </div>
              */}
+                </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default Projects;
