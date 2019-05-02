1277e670578a63aeb46785d2deb9fb9eed918c62
raitalharehman <talharaay@gmail.com>
2019-05-02 17:06:20 +0500

    write fist test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import Home from './components/pages/home';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders welcome message', () => {
+  const wrapper = shallow(<Home />);
+  const welcome = <h2>Talha Rehman</h2>;
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
