55d7a44f0ceaf2d5fee44408fc4ae89961550186
raitalharehman <talharaay@gmail.com>
2019-05-03 13:09:25 +0500

    SAVEPOINT

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -3,6 +3,24 @@ import React, { Component } from 'react';
 //components  
 import NavBar from '../nav-bar/nav-bar';
 
+function Project(props) {
+    const { img, title, url, text, tags, github } = props;
+    return <div className="project">
+        <img src={img} alt={title} />
+        <div className="project-text">
+            <h3>{title}
+                <a href={url} target="_blank" rel="noreferrer noopener"><i className="fas fa-link"></i></a>
+                {github ?
+                    <a href={github} target="_blank" rel="noreferrer noopener"><span className="fab fa-github"></span></a>
+                    : null
+                }
+            </h3>
+            <p>{text}</p>
+        </div>
+        {tags}
+    </div>
+}
+
 class Projects extends Component {
     componentWillReceiveProps() {
         document.title = "Projects - Rai Talha Rehman Khan";
@@ -18,35 +36,27 @@ class Projects extends Component {
                 <NavBar page="projects" />
                 <div className="projects-container">
                     <h2>Projects</h2>
-                    <div className="project">
-                        <img src="https://i.imgur.com/vQsy7iGh.png" alt="Pakixah" />
-                        <div className="project-text">
-                            <h3>Pakixah <a href="https://pakixah.com/" target="_blank" rel="noreferrer noopener"><i className="fas fa-link"></i></a></h3>
-                            <p>Its a Portal base on News, Blog, Forum, Social wall, Heros, Videos, Petitions & Polls</p>
-                        </div>
-                        <div className="project-tags">
+                    <Project img="https://i.imgur.com/vQsy7iGh.png" title="Pakixah" url="https://pakixah.com/"
+                        text="Its a Portal base on News, Blog, Forum, Social wall, Heros, Videos, Petitions & Polls"
+                        tags={<div className="project-tags">
                             <div className="tag tag-css">MaterializeCSS</div>
                             <div className="tag tag-js">JS</div>
                             <div className="tag tag-mysql">MySQL</div>
                             <div className="tag tag-html">Laravel</div>
-                        </div>
-                    </div>
+                        </div>}
+                    />
                     <h2>Contributions</h2>
-                    <div className="project">
-                        <img src="https://i.imgur.com/fnJ2p84h.png" alt="Saleor Store front" />
-                        <div className="project-text">
-                            <h3>Saleor Store front <a href="https://pwa.getsaleor.com/" target="_blank" rel="noreferrer noopener"><i className="fas fa-link"></i></a> <a href="https://github.com/mirumee/saleor-storefront" target="_blank" rel="noreferrer noopener"><span className="fab fa-github"></span></a></h3>
-                            <p>A GraphQL-powered, React, PWA, single-page application storefront for Saleor.</p>
-
-                        </div>
-                        <div className="project-tags">
+                    <Project img="https://i.imgur.com/fnJ2p84h.png" title="Saleor Store front" url="https://pwa.getsaleor.com/"
+                        text="A GraphQL-powered, React, PWA, single-page application storefront for Saleor."
+                        github="https://github.com/mirumee/saleor-storefront"
+                        tags={<div className="project-tags">
                             <div className="tag tag-pwa">PWA</div>
                             <div className="tag tag-react">React</div>
                             <div className="tag tag-js">Typescript</div>
                             <div className="tag tag-nodejs">Node JS</div>
                             <div className="tag tag-mysql">GraphQL</div>
-                        </div>
-                    </div>
+                        </div>}
+                    />
                 </div>
             </div>
         );
