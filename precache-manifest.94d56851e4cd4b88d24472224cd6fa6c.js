a83ac1d6d7eac54b7388841cca5703aa2384eb4a
Talha Rehman <talha.rehman@alchemative.com>
2022-07-04 23:08:41 +0500

    Updates

---
+++ precache-manifest.94d56851e4cd4b88d24472224cd6fa6c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b45c2095ff5a5eada4585e23988b1bd1",
+    "url": "/raitalharehman/index.html"
+  },
+  {
+    "revision": "e777d8093462a0cd3c33",
+    "url": "/raitalharehman/static/css/main.fc582bc9.chunk.css"
+  },
+  {
+    "revision": "0374170932c4953a8821",
+    "url": "/raitalharehman/static/js/2.efbbf3dc.chunk.js"
+  },
+  {
+    "revision": "e777d8093462a0cd3c33",
+    "url": "/raitalharehman/static/js/main.0a1a9cc6.chunk.js"
+  },
+  {
+    "revision": "c751480e5275d777f452",
+    "url": "/raitalharehman/static/js/runtime~main.ed092827.js"
+  },
+  {
+    "revision": "2d07687d8c04e0180834fff319532676",
+    "url": "/raitalharehman/static/media/github.2d07687d.svg"
+  },
+  {
+    "revision": "8f796fd22b82580cb9d16f202c070105",
+    "url": "/raitalharehman/static/media/linkedin.8f796fd2.svg"
+  },
+  {
+    "revision": "07a3c6f06d4652d6e7124156509097ac",
+    "url": "/raitalharehman/static/media/npm.07a3c6f0.jpg"
+  },
+  {
+    "revision": "f529bb5532d6292f4022e64ef12e1fa9",
+    "url": "/raitalharehman/static/media/overflow.f529bb55.svg"
+  },
+  {
+    "revision": "40f3347208f548533fb55e815498fa55",
+    "url": "/raitalharehman/static/media/twitter.40f33472.svg"
+  },
+  {
+    "revision": "ad5080eede43b064f74512a50018feb9",
+    "url": "/raitalharehman/static/media/uol.ad5080ee.svg"
+  }
+]);
